019bf2e8e2bbc60e314f59873b813a78f10f83d9
1-8192 <allegranzia@gmail.com>
2019-03-26 16:05:38 -0400

    Added some long and log out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: "users#home"
+
   resources :destinations, only: [:index, :show]
 
   resources :books, only: [:index, :show]
@@ -8,5 +10,7 @@ Rails.application.routes.draw do
   resources :users
 
   get 'login', to: 'sessions#new', as: 'login'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy', as: 'logout'
 
 end
