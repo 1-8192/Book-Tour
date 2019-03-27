e2e94d77f71bbb979c6f00f701e684c63263eb4e
1-8192 <allegranzia@gmail.com>
2019-03-27 10:35:23 -0400

    Create Review works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,18 +3,24 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
-    @book = Book.find(params[:review][:book_id])
+    @book = Book.find(params[:book_id])
   end
 
   def create
-
+    @review = Review.new(review_params)
+    @review.user_id = session[:user_id]
+    @review.save
+    redirect_to @review
   end
 
   def show
-
+    @review = Review.find(params[:id])
   end
 
+  private
 
-
+  def review_params
+    params.require(:review).permit(:book_id, :destination_id, :title, :review_body)
+  end
 
 end
