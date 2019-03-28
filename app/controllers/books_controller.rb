664709eab3078cf0142b48946b8dbc7c7c3a084e
merestrickland <mere.strickland@gmail.com>
2019-03-28 11:14:52 -0400

    css and faulty search

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,21 @@
 class BooksController < ApplicationController
 
   def index
-    @books = Book.all
+    @books = Book.search_by_title(params[:search])
+
   end
 
   def show
     @book = Book.find(params[:id])
     @review = Review.new
     @reviews = Review.where(book_id: @book.id)
+  end
+
+
+
+
+  def book_params
+    params.require(:book).permit(:title, :author, :description, :publication_date, :genre, :search)
   end
 
 end
